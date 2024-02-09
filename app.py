e9dfc7ac20d4a35dc045d97b879b93155c66105d
xFreshu <lprzybyszcontact@gmail.com>
2024-02-09 08:58:52 +0100

    fix

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
 from flask import Flask
-<<<<<<< HEAD
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
-=======
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -23,6 +11,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Import modeli
+
 from models import User
->>>>>>> 0f5e741 (create basic function which create dblite)
+
