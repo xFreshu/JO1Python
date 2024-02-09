8aa37196b79bc7308a66c300df11fdfbc5e82940
xFreshu <lprzybyszcontact@gmail.com>
2024-02-09 08:53:53 +0100

    create basic function which create dblite

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+<<<<<<< HEAD
 
 app = Flask(__name__)
 
@@ -10,3 +11,18 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
+=======
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sekretnyKlucz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Import modeli
+from models import User
+>>>>>>> 0f5e741 (create basic function which create dblite)
